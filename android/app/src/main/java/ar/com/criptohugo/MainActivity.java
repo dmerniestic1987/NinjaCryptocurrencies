d9742b336d7345798aa1b5279682a1ec8a826824
diegomernies <diego.alejandro.mernies@gmail.com>
2018-02-28 19:41:33 -0300

    DAM - Se crea gramegneto para listar criptomonedas

--- android/app/src/main/java/ar/com/criptohugo/MainActivity.java
+++ android/app/src/main/java/ar/com/criptohugo/MainActivity.java
@@ -1,14 +1,20 @@
 package ar.com.criptohugo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import ar.com.criptohugo.fragments.CryptoCurrenciesFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -32,6 +38,51 @@ public class MainActivity extends AppCompatActivity {
     };
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        Context context = getApplicationContext();
+        CharSequence text = "";
+        int duration = Toast.LENGTH_SHORT;
+
+
+
+        if (id == R.id.action_developer_info) {
+            Log.i(TAG, "developer info");
+            Toast toast = Toast.makeText(context, R.string.action_developer_info, duration);
+            toast.show();
+            return true;
+        }
+
+        if (id == R.id.action_help) {
+            Log.i(TAG, "Ayuda");
+            Toast toast = Toast.makeText(context, R.string.action_help, duration);
+            toast.show();
+            return true;
+        }
+
+        if (id == R.id.action_terms_conditions) {
+            Log.i(TAG, "Términos y Condiciones");
+            Toast toast = Toast.makeText(context, R.string.action_terms_conditions, duration);
+            toast.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -39,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }
