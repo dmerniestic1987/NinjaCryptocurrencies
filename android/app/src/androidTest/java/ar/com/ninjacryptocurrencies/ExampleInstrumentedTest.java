530e7d756c445578c1e31fb5c2fcd0fb0feee997
diegomernies <diego.alejandro.mernies@gmail.com>
2018-05-19 21:19:37 -0300

    DAM - Se corrigen estructuras de paquetes y se agregan dependencias para firebase

--- android/app/src/androidTest/java/ar/com/ninjacryptocurrencies/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/ar/com/ninjacryptocurrencies/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("ar.com.criptohugo", appContext.getPackageName());
+        assertEquals("ar.com.ninjacryptocurrencies", appContext.getPackageName());
     }
 }
