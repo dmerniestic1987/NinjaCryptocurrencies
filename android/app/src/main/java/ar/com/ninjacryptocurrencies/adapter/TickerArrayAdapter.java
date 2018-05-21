a593bdbd732b26751924795177a6188256b4c32e
diegomernies <diego.alejandro.mernies@gmail.com>
2018-05-21 15:22:01 -0300

    DAM - Se corrigen pantallas

--- android/app/src/main/java/ar/com/ninjacryptocurrencies/adapter/TickerArrayAdapter.java
+++ android/app/src/main/java/ar/com/ninjacryptocurrencies/adapter/TickerArrayAdapter.java
@@ -1,15 +1,19 @@
 package ar.com.ninjacryptocurrencies.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
 import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.List;
 import java.util.Locale;
 
@@ -54,19 +58,47 @@ public class TickerArrayAdapter extends ArrayAdapter<Ticker> {
 
         itemView.setId(bean.getId().hashCode());
         TextView titleTicker = itemView.findViewById(R.id.titleTicker);
-        titleTicker.setText(bean.getName());
+        titleTicker.setText(bean.getName() + " (" + bean.getSymbol() + ")");
 
-        NumberFormat format = NumberFormat.getInstance(Locale.ENGLISH);
-        format.setMinimumFractionDigits(2);
-        format.setMaximumFractionDigits(6);
+        NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("es_AR"));
+        format.setCurrency(Currency.getInstance("USD"));
+        String result = format.format(bean.getPriceUsdBigDecimal());
         TextView valueUsdPrice = itemView.findViewById(R.id.valueUsdPrice);
-        valueUsdPrice.setText(format.format(bean.getPriceUsdBigDecimal()));
+        valueUsdPrice.setText(result);
 
         NumberFormat formatBtc = NumberFormat.getInstance(Locale.ENGLISH);
         format.setMinimumFractionDigits(1);
         format.setMaximumFractionDigits(9);
         TextView valueBtcPrice = itemView.findViewById(R.id.valueBtcPrice);
         valueBtcPrice.setText(formatBtc.format(bean.getPriceBTCBigDecimal()));
+
+
+        TextView text1Hs = itemView.findViewById(R.id.text1hs);
+        text1Hs.setText(bean.getPercentChange1h() + " %");
+        text1Hs.setTextColor(ContextCompat.getColor(this.getContext(), R.color.greenPrimaryDark));
+        if (bean.getPercentChange1h().startsWith("-")){
+            text1Hs.setTextColor(ContextCompat.getColor(this.getContext(), R.color.red_warning));
+        }
+
+        TextView text24Hs = itemView.findViewById(R.id.text24hs);
+        text24Hs.setText(bean.getPercentChange24h() + " %");
+        text24Hs.setTextColor(ContextCompat.getColor(this.getContext(), R.color.greenPrimaryDark));
+        if (bean.getPercentChange24h().startsWith("-")){
+            text24Hs.setTextColor(ContextCompat.getColor(this.getContext(), R.color.red_warning));
+        }
+
+
+        TextView text7days = itemView.findViewById(R.id.text7days);
+        text7days.setText(bean.getPercentChange7d() + " %");
+        text7days.setTextColor(ContextCompat.getColor(this.getContext(), R.color.greenPrimaryDark));
+        if (bean.getPercentChange7d().startsWith("-")){
+            text7days.setTextColor(ContextCompat.getColor(this.getContext(), R.color.red_warning));
+        }
+
+        TextView textCirculating = itemView.findViewById(R.id.textCirculating);
+        textCirculating.setText(bean.getAvailableSupply() + " (" + bean.getSymbol() + ")");
+
+
         return itemView;
     }
 
