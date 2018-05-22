2afe9353015c51cfb8cd0b03b978e9bde7b2f4e5
diegomernies <diego.alejandro.mernies@gmail.com>
2018-05-22 17:27:40 -0300

    DAM - agrega acerca de y se corrigen funcionalidades

--- android/app/src/main/java/ar/com/ninjacryptocurrencies/activity/AbooutAppActivity.java
+++ android/app/src/main/java/ar/com/ninjacryptocurrencies/activity/AbooutAppActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import ar.com.ninjacryptocurrencies.BuildConfig;
 import ar.com.ninjacryptocurrencies.R;
 
 public class AbooutAppActivity extends AppCompatActivity implements View.OnClickListener{
-    private Button aceptar, cancelar;
+    private Button aceptar;
     private static String TAG = "AbooutApp";
+    private TextView txt_app_version;
+    private TextView txt_version_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,28 +28,20 @@ public class AbooutAppActivity extends AppCompatActivity implements View.OnClick
         this.aceptar = this.findViewById(R.id.btn_aceptar_ninja);
         aceptar.setOnClickListener(this);
 
-        this.cancelar = this.findViewById(R.id.btn_cancelar_ninja);
-        cancelar.setOnClickListener(this);
-
+        this.txt_app_version = this.findViewById(R.id.txt_app_version);
+        txt_app_version.setText(getString(R.string.display_app_version) + " " + String.valueOf(BuildConfig.VERSION_CODE));
+        txt_version_name = this.findViewById(R.id.txt_version_name);
+        txt_version_name.setText(getString(R.string.display_version_name) + " " + String.valueOf(BuildConfig.VERSION_NAME));
+        
     }
 
     @Override
     public void onClick(View view) {
+
         if (view.getId() == R.id.btn_aceptar_ninja){
-            aceptar();
+            Log.d(TAG, "About OK clicked");
             this.finish();
         }
 
-        if (view.getId() == R.id.btn_cancelar_ninja){
-            this.finish();
-        }
-    }
-
-    /**
-     * Realiza algo con el aceptar
-     */
-    private void aceptar() {
-        Log.d(TAG, "Aceptar");
-        Toast.makeText(this,"Ha aceptado los térmios y condiciones", Toast.LENGTH_LONG).show();
     }
 }
