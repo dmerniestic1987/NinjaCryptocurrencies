fe5165f02591c41607c54867a5555c30c073881e
diegomernies <diego.alejandro.mernies@gmail.com>
2018-05-27 23:34:19 -0300

    DAM - Se corrige código

--- android/app/src/main/java/ar/com/ninjacryptocurrencies/fragment/TickerDetailFragment.java
+++ android/app/src/main/java/ar/com/ninjacryptocurrencies/fragment/TickerDetailFragment.java
@@ -10,6 +10,11 @@ import android.widget.TextView;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
 import ar.com.ninjacryptocurrencies.R;
 import ar.com.ninjacryptocurrencies.analytics.CryptoCurrenciesAnalytics;
 import ar.com.ninjacryptocurrencies.bean.Ticker;
@@ -57,23 +62,53 @@ public class TickerDetailFragment extends Fragment {
         TextView symbolTickerDetail = view.findViewById(R.id.symbolTickerDetail);
         symbolTickerDetail.setText(this.ticker.getSymbol());
 
-        TextView valueUsdPrice = view.findViewById(R.id.valueUsdPrice);
-        valueUsdPrice.setText(this.ticker.getPriceUsd());
+        NumberFormat format = NumberFormat.getCurrencyInstance(new Locale("es_AR"));
+        format.setCurrency(Currency.getInstance("USD"));
 
+        //************ PRICE USD
+        String priceUsd = "";
+        if (this.ticker.getPriceUsd() != null)
+            priceUsd = format.format(new BigDecimal(this.ticker.getPriceUsd()));
+
+        TextView valueUsdPrice = view.findViewById(R.id.valueUsdPrice);
+        valueUsdPrice.setText(priceUsd);
+
+        //************ PRICE BTC
         TextView valueBtcPrice = view.findViewById(R.id.valueBtcPrice);
         valueBtcPrice.setText(this.ticker.getPriceBtc());
 
-        TextView valueMarketCapUsd = view.findViewById(R.id.valueMarketCapUsd);
-        valueMarketCapUsd.setText(this.ticker.getMarketCapUsd());
+        //*********** CAPITAL MARKET
+        String marketCapUsd = "";
+        if (this.ticker.getMarketCapUsd() != null)
+            marketCapUsd = format.format(new BigDecimal(this.ticker.getMarketCapUsd()));
 
-        TextView valueTotalSupply = view.findViewById(R.id.valueTotalSupply);
-        valueTotalSupply.setText(this.ticker.getTotalSupply());
+        TextView valueMarketCapUsd = view.findViewById(R.id.valueMarketCapUsd);
+        valueMarketCapUsd.setText(marketCapUsd);
+
+        //*********** VOL 24 HS
+        String vol24hs = "";
+        if (this.ticker.getV24hVolumeUsd() != null)
+            vol24hs = format.format(new BigDecimal(this.ticker.getV24hVolumeUsd()));
 
         TextView valueVol24hs = view.findViewById(R.id.valueVol24hs);
-        valueVol24hs.setText(this.ticker.getV24hVolumeUsd());
+        valueVol24hs.setText(vol24hs);
 
+        //*********** CIRCULACION
+        format = NumberFormat.getInstance(new Locale("es_AR"));
+        String totalSupply = "";
+        if (this.ticker.getTotalSupply() != null){
+            totalSupply = format.format(new BigDecimal(this.ticker.getTotalSupply()));
+        }
+        TextView valueTotalSupply = view.findViewById(R.id.valueTotalSupply);
+        valueTotalSupply.setText(totalSupply + " " + this.ticker.getSymbol());
+
+        //*********** CANTIDAD MAXIMA
+        String maxSupply = "";
+        if (this.ticker.getMaxSupply() != null){
+            maxSupply = format.format(new BigDecimal(this.ticker.getMaxSupply()));
+        }
         TextView valueMaxSupply = view.findViewById(R.id.valueMaxSupply);
-        valueMaxSupply.setText(this.ticker.getMaxSupply());
+        valueMaxSupply.setText(maxSupply + " " + this.ticker.getSymbol());
     }
 
     @Override
