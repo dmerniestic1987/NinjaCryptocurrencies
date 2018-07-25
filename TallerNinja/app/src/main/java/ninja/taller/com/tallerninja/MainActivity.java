cef8b3231a9b5e8bc19c6675e24ba2d3c6cf05e0
Diego <dmernies@uade.edu.ar>
2018-07-24 22:00:43 -0300

    DAM - Se agrega ejemplo de TallerNinja

---
+++ TallerNinja/app/src/main/java/ninja/taller/com/tallerninja/MainActivity.java
@@ -0,0 +1,65 @@
+package ninja.taller.com.tallerninja;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Sarlanga Marlanga", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Log.d("TAGTALLER", " CLICK EN SARLANGA MARLANGA");
+
+
+            }
+        });
+
+        Log.d("TAGTALLER", " * * * *Estamos en onCreate()");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        Log.d("TAGTALLER", "onCreateMenuOption ** ");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            Log.d("TAGTALLER", "CLICK EN SETTINGS ");
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
